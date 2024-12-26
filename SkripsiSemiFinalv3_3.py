8268dfd3fe5426ad82fae841e230ae791ed2389e
Panda-Bulat <levistonnuts@gmail.com>
2024-12-26 16:08:57 +0700

    Update SkripsiSemiFinalv3_3.py

--- SkripsiSemiFinalv3_3.py
+++ SkripsiSemiFinalv3_3.py
@@ -213,14 +213,14 @@ def main():
     st.subheader("Step 2: Pilih bahan alergen")
     user_allergens = st.text_input("Masukkan bahan alergen (dipisah tanda koma (,) misal: telur, udang)")
     st.markdown("""
-        <p style="font-size: 10px; margin-bottom: 4px;">*Jika terdapat typo pada input alergen, hasil rekomendasi bisa saja tidak akurat.</p>
-        <p style="font-size: 10px; margin-bottom: 4px;">*Jika tidak ada, silakan masukkan tanda minus (-).</p>""", unsafe_allow_html=True)
+        <p style="font-size: 12px; margin-top: 5px; margin-bottom: 4px;">*Jika terdapat typo pada input alergen, hasil rekomendasi bisa saja tidak akurat.</p>
+        <p style="font-size: 12px; margin-top: 5px; margin-bottom: 4px;">*Jika tidak ada, silakan masukkan tanda minus (-).</p>""", unsafe_allow_html=True)
 
     # Step 3: User enters ingredients
     st.subheader("Step 3: Pilih bahan makanan yang ingin dimasukkan")
     user_ingredients = st.text_input("Masukkan bahan makanan (dipisah tanda koma (,) misal: telur, udang)")
     st.markdown("""
-        <p style="font-size: 10px; margin-bottom: 5px;">*Jika terdapat typo pada input bahan makanan, hasil rekomendasi bisa saja berbeda.</p>""", unsafe_allow_html=True)
+        <p style="font-size: 12px; margin-top: 5px; margin-bottom: 7px;">*Jika terdapat typo pada input bahan makanan, hasil rekomendasi bisa saja berbeda.</p>""", unsafe_allow_html=True)
 
     # Submit button
     if st.button("Submit"):
